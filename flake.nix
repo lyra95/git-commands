ef3acaf4ee0d894ba2540e171fefa4f3621a15bc
jo <95hyouka@gmail.com>
2023-06-24 23:16:21 +0900

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default =
+        let
+          toolchain = fenix.packages.${system}.minimal.toolchain;
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+
+        (pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        }).buildRustPackage {
+          pname = "git-commands";
+          version = "0.2.0";
+
+          src = ./.;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+    });
+}
